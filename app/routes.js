147301f861577bd692904fb0867b6fc626328208
tegucigalpa <tegucigalpa@tegucigalpas-MacBook-Air.local>
2018-11-27 22:39:52 -0600

    commit

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,14 @@ module.exports = function(app, passport) {
     app.get('/', function(req, res){
      res.render('index.ejs');
     });
+
+    app.get('/registro', function(req, res){
+        res.render('registro.ejs', {message: req.flash('signupMessage')});
+    });
+
+    app.get('/sesion_inicio', function(req, res){
+       res.render('sesion_inicio.ejs', {message: req.flash('signupMessage')});
+    });
 };
 
     function isLoggedIn(req, res, next){
